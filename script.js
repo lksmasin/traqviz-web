e4682f75ed94131b6cd80e24fd39a30c1f581f90
lksmasin <lksmasin@gmail.com>
2024-12-31 19:48:18 +0100

    Web created

---
+++ script.js
@@ -0,0 +1,19 @@
+// Funkce pro otevření aplikace
+function openApp() {
+    window.open("https://lksmasin.github.io/trackifly/", "_blank");  // Otevře webovou stránku aplikace
+}
+
+// Funkce pro stažení aplikace
+function downloadApp() {
+    window.open("https://github.com/lksmasin/trackifly", "_blank");  // Otevře Google Play Store
+}
+
+// Funkce pro změnu obrázku při najetí myší
+function changeImage(isHover) {
+    const screenshot = document.getElementById('app-screenshot');
+    if (isHover) {
+        screenshot.src = "./screen2.png"; // Nový obrázek při hoveru
+    } else {
+        screenshot.src = "./screen1.png"; // Původní obrázek
+    }
+}
